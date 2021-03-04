320c679ea75107010a09737234946899b2f92774
Edson <edson.fullstack@gmail.com>
2021-03-04 19:52:51 -0300

    api base

--- source/routes/api.py
+++ source/routes/api.py
@@ -5,86 +5,35 @@ from fastapi import FastAPI, Request
 
 
 def access(db, table, method):
-    if table is not None and db['apis'].count(table=table) > 0 and list(db['apis'].find(table=table))[0][method]:
-        return True
-    else:
-        # fix after test
-        return False
+    return True
 
 
-def anti_injection(params):
-    words = ["$", "*", "SELECT", "DROP"]
-    for w in words:
-        params = params.replace(w, "")
-    return params
-
-
-async def url_query(request):
-    params = anti_injection(request.url.query)
-    statement = params.replace("&", " AND ").replace("%22", "") \
-        .replace("%27", "'").replace("|", " OR ").replace("%20", " ")
-    return statement
-
-
-async def define_api(db):
-    if not db['apis']:
-        table = db.create_table('apis',
-                                primary_id='table',
-                                primary_type=db.types.string(25))
-        table.create_column('table', db.types.string).create_column('get', db.types.boolean) \
-            .create_column('post', db.types.boolean).table.create_column('put', db.types.boolean) \
-            .create_column('delete', db.types.boolean)
-        db.commit()
-        return table
-    else:
-        return {"configure": "success"}
-
-
-def init_app(app, sys, access_point="/api", db_type="postgres"):
+def init_app(app, sys, access_point="/api"):
     # define postgres,mysql as attributes of the object
-    db = getattr(sys, db_type)
+    db = getattr(sys)
 
     @app.get(access_point, tags=[access_point])
     async def api():
-        return await define_api(db)
+        return True
 
     @app.get(access_point + "/{table}", tags=[access_point])
     async def get(table, request: Request):
         """all elements on a table"""
-        if access(db, table, 'get'):
-            statement = await url_query(request)
-            return await get_list(statement, table)
-        else:
-            return {"access": "denied"}
+        return True
 
     async def get_list(statement, table):
-        if statement:
-            try:
-                return list(db[table].find(text(statement)))
-            except Exception as e:
-                return "query invalid:" + statement + "-" + e
-        else:
-            return list(db[table].all())
+        return True
 
     @app.post(access_point + "/{table}", tags=[access_point])
     async def post(table, request: Request):
-        data = dict(await request.json())
-        result = db[table].insert(data)
-        return {result: data}
+        return True
 
     @app.put(access_point + "/{table}", tags=[access_point])
     async def put(table, request: Request):
-        statement = await url_query(request)
-        data = dict(await request.json())
-        if statement != "":
-            result = db[table].update(data, keys=statement.split("=")[0])
-        return result
+        return True
 
     @app.delete(access_point + "/{table}", tags=[access_point])
     async def delete(table, request: Request):
-        statement = await url_query(request)
-        if statement != "":
-            result = db[table].delete(text(statement))
-        return result
+        return True
 
     return app
